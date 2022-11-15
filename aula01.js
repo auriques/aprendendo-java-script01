8d3fddcd2f680df000d5b244762bcf247b66f3c3
Auriques <marcos_ouriques@hotmail.com>
2022-11-15 20:43:43 -0300

    primeira aula de JS

---
+++ aula01.js
@@ -0,0 +1,15 @@
+/* Aula 01 dio - desafio 01 
+Fazer um programa para calcular o valor do combustivel em uma viagem
+serão 3 variaveis preco, valor medio de gasto do carro e distancia
+let atribui variavel que pode mudar ao decorrer do programa
+const atribui variavel que não muda no decorrer do programa
+.tofixed(2) - coloca duas casa apos a virgula
+*/
+let preco = 5.79;
+let vlmedio = 10;
+let distancia=100;
+let gastos = ((distancia/vlmedio)*preco)
+
+
+
+console.log(gastos.toFixed(2)) 
